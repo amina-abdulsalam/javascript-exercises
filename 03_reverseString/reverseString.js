3ba6e928dc31f8eab94180bf99b6c3e0c8c13363
aoabduls <aoabduls@ncsu.edu>
2023-11-16 17:17:39 -0500

    Finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = "";
 
+    for(i = string.length; i > 0; i--) {
+        let piece = string.substring(i-1, i);
+        reversedString = reversedString.concat(piece);
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
