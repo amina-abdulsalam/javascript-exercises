4569c4d0b8a374df2b1bac9227f1526ce3aea232
aoabduls <aoabduls@ncsu.edu>
2023-11-16 17:55:04 -0500

    Working on exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(numsArray, ...args) {
+    let returnArray = [];
+    
+    for(i = 0; i < args.length; i++) {
+        let removeNum = args[i];
+        
+        // issue: printing out valid array elements each time instead of once
+        for(j = 0; j < numsArray.length; j++) {
+            let element = numsArray[j];
 
+            if(element != removeNum) {
+                returnArray.push(element);
+            }
+        }
+    }
+    
+    return returnArray;
+    
 };
 
 // Do not edit below this line
