54e1f5f1f98fb22dd8adf703f561e264a020594b
aoabduls <aoabduls@ncsu.edu>
2023-11-16 17:06:20 -0500

    Finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let returnString = "";
+    
+    if(num >= 0) {
+        for(i = 0; i < num; i++) {
+            returnString = returnString.concat(string);
+        } 
+    } else {
+        returnString = returnString.concat("ERROR");
+    }
+    
+    return returnString;   
 };
 
 // Do not edit below this line
